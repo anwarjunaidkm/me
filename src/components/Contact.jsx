2da06b3f51c4550e2e972c5e32d3f20d2e264363
anwarjunaidkm <anverjunaidvilayil@gmail.com>
2023-03-28 17:44:33 +0530

    cv

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div name='contact' className='bg-[#0a192f] w-full h-screen flex justify-center items-center p-4'>
+    <div name='contact' className='bg-[#0a192f] w-full h-auto flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/56d599d6-d694-48ff-9aaf-ce1ed9cb6148" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p  className='text-4xl font-bold inline  border-b-4 border-pink-600 text-gray-300'>
                     Contact
                 </p>
-                <p className='text-gray-300 py-4'>Submit the form below or shoot me an email-ajvly22@gmail.com</p>
+                <p className='text-gray-300 py-4'>Submit the form below or shoot me an Email : ajvly22@gmail.com</p>
             </div>
             <input className='p-2 bg-[ccd6f6]' type="text" placeholder='Name' name='name' />
             <input className='p-2 my-4 bg-[ccd6f6]' type="email" placeholder='Email' name='email' />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -108,7 +108,7 @@ function Navbar() {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href=""
+              href="https://drive.google.com/file/d/12BalljkfUePGlqNTQ2Y9WfwyBwNbSpXP/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
